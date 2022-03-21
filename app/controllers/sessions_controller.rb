1cf96396a2589f1717c366c2d98b88d003fee1bc
Harshu2306 <harshpatelvi.dev@gmail.com>
2022-03-21 14:25:08 +0530

    add authentication system and build sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,29 @@ class SessionsController < ApplicationController
 
 	end
 
+	def create
+
+		user = User.find_by(username: params[:session][:username])
+		if user && user.authenticate(params[:session][:password])
+			session[:user_id] = user.id
+			flash[:success] = "You Have Logged in successfully."
+			redirect_to root_path
+
+		else
+			flash.now[:error] = "OOPS!! You Have Enter Wrong Email or Password."
+			render 'new'
+
+		end
+
+	end
+
+
+	def destroy
+
+		session[:user_id] = nil
+		flash[:success] = "You Have Loggedout Successfully."
+		redirect_to login_path
+	end
 
 
 end
