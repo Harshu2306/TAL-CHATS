13eeabd76b80dc157fd53dc9e0ba14b770856eb2
Harshu2306 <harshpatelvi.dev@gmail.com>
2022-03-21 20:42:51 +0530

    added Actioncable broadcast method in contoller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,7 +5,7 @@ before_action :require_user
 
 		message = current_user.messages.build(message_params)
 		if message.save
-			redirect_to root_path
+			ActionCable.server.broadcast "chatroom_channel", foo: message.body
 		end
 	end
 
